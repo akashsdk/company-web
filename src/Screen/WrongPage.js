6c3056a4ed08f1b3838a38d28619e99d1a0b14c1
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-06 18:24:50 +0600

    ok

--- src/Screen/WrongPage.js
+++ src/Screen/WrongPage.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import './WrongPage.css';
+import React from "react";
+import "./WrongPage.css";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
 export default function WrongPage() {
   return (
-    <div>WrongPage</div>
-  )
+    <div className="WrongPage-Body">
+      <Result status="500" />
+      <p>Sorry, We could not find the page! Something went wrong.</p>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button type="primary" danger>
+          Back Home
+        </Button>
+      </Link>
+    </div>
+  );
 }
