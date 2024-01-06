b32fd1b6c1aec58e8c8ccf4157f93a95a93f80d4
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-06 18:02:37 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import WrongPage from "./Screen/WrongPage";
+import Home from "./Screen/Home";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Contacts from './Screen/Contacts';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="*" element={<WrongPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Contacts" element={<Contacts />} />
+        </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
