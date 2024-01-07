0781ce0c805eccdb0bd089fd258b1ff8e04f0b02
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-08 00:21:50 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,19 +1,53 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 import { FloatButton } from "antd";
 
-import HomeAnimation from '../Components/HomeAnimation';
+import HomeAnimation from "../Components/HomeAnimation";
+
+import img1 from "../Image/We-Deploy-World.png";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className='home-Body'>
-      <div className='homeBody'>
+    <div className="home-Body">
+      <div className="homeBody">
         {/* <HomeAnimation/> */}
-      <div className='homeMain-Body'>
-        <div>Left</div>
-        <div>right</div>
-      </div>
+        <div className="homeMain-Body">
+          <div className="homeMain-Left">
+            <p>Offshore Software Development 🙌</p>
+            <p>We Deploy World Class Agile Product Teams on Demand</p>
+            <p>
+              Vivasoft specializes in providing custom software development
+              services. Get premium software solutions from a reliable
+              outsourcing partner and set your business apart. Skilled teams
+              that can design, build, space and scale your vision in the most
+              efficient way.
+            </p>
+            <div>
+              <div>
+                <p>3+</p>
+                <p>Years of experience</p>
+              </div>
+
+              <div>
+                <p>10+</p>
+                <p>Dedicated Engineers</p>
+              </div>
+
+              <div>
+                <p>11+</p>
+                <p>Successful Projects</p>
+              </div>
+            </div>
+            <Link>
+              <p>Hire Developers</p>
+            </Link>
+          </div>
+          <div className="homeMain-Right">
+            <img className="homeMain-Right-Img" src={img1} alt="SVG" />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
