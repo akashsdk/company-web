c6432b5025d5769d6726aeb259db34daa4455133
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-11 09:01:25 +0600

    ok

--- src/Cart/ServicesCart.js
+++ src/Cart/ServicesCart.js
@@ -4,25 +4,37 @@ import "../Screen/Home.css";
 import { Link } from "react-router-dom";
 
 import img1 from "../Image/team-works.png";
+import img2 from "../Image/Work-removebg-preview.png";
 
 export default function ServicesCart(props) {
   return (
-    <div className="homeMain-Body">
-      <div className="homeMain-Left">
-        <p className="homeMain-Left-Text1"> {props.text1} 🙌</p>
-        <p className="homeMain-Left-Text2">
-          {props.text2}
-        </p>
-        <p className="homeMain-Left-Text3">
-          {props.text3}
-        </p>
+    <div className="ServicesCart-Body">
+      <div className="homeMain-Body">
+        <div className="homeMain-Left">
+          <p className="homeMain-Left-Text1"> {props.text1} 🙌</p>
+          <p className="homeMain-Left-Text2">{props.text2}</p>
+          <p className="homeMain-Left-Text3">{props.text3}</p>
 
-        <Link to="/Contacts" className="homeMain-Left-Link">
-          <p className="homeMain-Left-Text6">Hire Developers</p>
-        </Link>
+          <Link to="/Contacts" className="homeMain-Left-Link">
+            <p className="homeMain-Left-Text6">Hire Developers</p>
+          </Link>
+        </div>
+        <div className="homeMain-Right">
+          <img className="homeMain-Right-Img" src={img1} alt="SVG" />
+        </div>
       </div>
-      <div className="homeMain-Right">
-        <img className="homeMain-Right-Img" src={img1} alt="SVG" />
+      <div className="home-Part-Div">
+        <p className="home-Part-Text1">{props.text4}</p>
+        <p className="home-Part-Text2">{props.text5}</p>
+      </div>
+      <div className="ServicesCart-Box">
+        <div className="homeMain-Left">
+          <p className="ServicesCart-Text1"> {props.text6} </p>
+          <p className="ServicesCart-Text2">{props.text7}</p>
+        </div>
+        <div className="homeMain-Right">
+          <img className="homeMain-Right-Img" src={img2} alt="SVG" />
+        </div>
       </div>
     </div>
   );
--- src/SemiScreen/TeamAugmentation.js
+++ src/SemiScreen/TeamAugmentation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./TeamAugmentation.css";
 import ServicesCart from "../Cart/ServicesCart";
+import HelpCart from "../Cart/HelpCart";
 
 export default function TeamAugmentation() {
   return (
@@ -10,7 +11,15 @@ export default function TeamAugmentation() {
           text1="IT Staff Augmentation Services"
           text2="Get Expert Resources, Maximize Your Team Performance Through Augmentation Strategies"
           text3="Looking for top-tier IT staff augmentation services? (web site name) has you covered. We offer comprehensive team augmentation solutions with highly skilled personnel to help you grow and strengthen your team. From software developers to project managers, we have the expertise to elevate your business to the next level. Partner with (web site name) today and watch your team thrive."
+          text4="About Team Augmentation Services"
+          text5="Do not let understaffing hold you back. Maximize your teams performance and reach your business goals with the best IT Staff Augmentation services from one of the most promising IT Staffing Companies. We provide the resources and support you need to keep your project on track and meet your deadlines."
+          text6="The Agile Solution for Business Growth: How Team Augmentation Can Help You Succeed"
+          text7="Team augmentation is a powerful outsourcing process that enables businesses to expand their team with skilled tech professionals on an on-demand basis. Whether you are a software company looking to access tech talent from around the world or a business seeking to quickly boost your teams capabilities, team augmentation can help you achieve your goals without the hassle of recruiting and onboarding full-time employees.
+
+          By tapping into specialized talent and expertise, team augmentation provides businesses with a unique solution to stay ahead of the competition and scale their business with ease. This effective strategy allows companies to reach their business goals on schedule and within budget while maintaining the flexibility and agility to adapt to changing business needs. With team augmentation, businesses can confidently take on new projects, expand their services, and deliver exceptional results to their clients."
         />
+
+        <HelpCart />
       </div>
     </div>
   );
