dbacc12ace60c769394776d85be8e1e795ff42eb
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-12 22:09:37 +0600

    ok

--- src/Screen/CheckOut.js
+++ src/Screen/CheckOut.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import './CheckOut.css';
 
+import Footer from '../Components/Footer';
+
 export default function CheckOut() {
   return (
-    <div>CheckOut</div>
+    <div className="AppBody">
+      <div className="CheckOut-Body">
+        <p>CheckOut</p>
+        <Footer />
+      </div>
+    </div>
   )
 }
--- src/Screen/Contacts.js
+++ src/Screen/Contacts.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "./Contacts.css";
 
+import Footer from "../Components/Footer";
+
 import Image1 from "../Image/Group-1000002614-1.png";
+
 import { Modal, message } from "antd";
 
 export default function Contacts() {
@@ -62,131 +65,135 @@ export default function Contacts() {
   return (
     <div className="AppBody">
       {contextHolder}
-      <div className="contacts-Body">
-        <div className="contacts-Left">
-          <p className="contacts-Text1">Contact Us</p>
-          <p className="contacts-Text2">
-            Fill in the below form to receive a detailed estimation.
-            <br />
-            One of our friendly team member will be in touch soon
-          </p>
-          <div className="contacts-Box1">
-            <div className="contacts-Box2">
-              <p className="contacts-Text3">Name:</p>
-              <p className="contacts-Text4">*</p>
-            </div>
-            <div className="contacts-Box3">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={handleChange}
-                className="contacts-Input"
-                placeholder="Your Good Name"
-              />
-            </div>
-          </div>
-
-          <div className="contacts-Box5">
-            <div className="contacts-Box7">
+      <div className="contacts-Body1">
+        <div className="contacts-Body">
+          <div className="contacts-Left">
+            <p className="contacts-Text1">Contact Us</p>
+            <p className="contacts-Text2">
+              Fill in the below form to receive a detailed estimation.
+              <br />
+              One of our friendly team member will be in touch soon
+            </p>
+            <div className="contacts-Box1">
               <div className="contacts-Box2">
-                <p className="contacts-Text3">Email:</p>
+                <p className="contacts-Text3">Name:</p>
                 <p className="contacts-Text4">*</p>
               </div>
-              <div className="contacts-Box6">
+              <div className="contacts-Box3">
                 <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={email}
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
                   onChange={handleChange}
-                  className="contacts-Input2"
-                  placeholder="Enter Email Id"
+                  className="contacts-Input"
+                  placeholder="Your Good Name"
                 />
               </div>
             </div>
 
-            <div className="contacts-Box7">
+            <div className="contacts-Box5">
+              <div className="contacts-Box7">
+                <div className="contacts-Box2">
+                  <p className="contacts-Text3">Email:</p>
+                  <p className="contacts-Text4">*</p>
+                </div>
+                <div className="contacts-Box6">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                    className="contacts-Input2"
+                    placeholder="Enter Email Id"
+                  />
+                </div>
+              </div>
+
+              <div className="contacts-Box7">
+                <div className="contacts-Box2">
+                  <p className="contacts-Text3">Phone:</p>
+                </div>
+                <div className="contacts-Box6">
+                  <input
+                    type="tel"
+                    id="phone"
+                    name="phone"
+                    pattern="[0-9]*"
+                    value={phone}
+                    onChange={handleChange}
+                    className="contacts-Input2"
+                    placeholder="Enter Phone No"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="contacts-Box1">
               <div className="contacts-Box2">
-                <p className="contacts-Text3">Phone:</p>
+                <p className="contacts-Text3">Services:</p>
+                <p className="contacts-Text4">*</p>
               </div>
-              <div className="contacts-Box6">
-                <input
-                  type="tel"
-                  id="phone"
-                  name="phone"
-                  pattern="[0-9]*"
-                  value={phone}
+              <div className="contacts-Box3">
+                <select
+                  className="contacts-Input"
+                  id="gender"
+                  name="gender"
+                  value={gender}
                   onChange={handleChange}
-                  className="contacts-Input2"
-                  placeholder="Enter Phone No"
+                >
+                  <option value="1">Team Augmentation</option>
+                  <option value="2">Offshore Office Expansion</option>
+                  <option value="3">MVP Service</option>
+                  <option value="4">End to End Development</option>
+                  <option value="5">Others</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="contacts-Box1">
+              <div className="contacts-Box2">
+                <p className="contacts-Text3">Message:</p>
+                <p className="contacts-Text4">*</p>
+              </div>
+              <div className="contacts-Box4">
+                <textarea
+                  id="text"
+                  name="text"
+                  value={text}
+                  onChange={handleChange}
+                  className="contacts-textarea"
                 />
               </div>
             </div>
-          </div>
 
-          <div className="contacts-Box1">
-            <div className="contacts-Box2">
-              <p className="contacts-Text3">Services:</p>
-              <p className="contacts-Text4">*</p>
-            </div>
-            <div className="contacts-Box3">
-              <select
-                className="contacts-Input"
-                id="gender"
-                name="gender"
-                value={gender}
-                onChange={handleChange}
+            <div className="contacts-Button-Div">
+              <button
+                className="contacts-Button1"
+                type="button"
+                onClick={showModal}
               >
-                <option value="1">Team Augmentation</option>
-                <option value="2">Offshore Office Expansion</option>
-                <option value="3">MVP Service</option>
-                <option value="4">End to End Development</option>
-                <option value="5">Others</option>
-              </select>
+                <p className="contacts-Button-Text">Reset</p>
+              </button>
+              <button className="contacts-Button2" onClick={openMessage}>
+                <p className="contacts-Button-Text">Send</p>
+              </button>
             </div>
           </div>
+          <Modal
+            title="Confirm to Reset !"
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          ></Modal>
 
-          <div className="contacts-Box1">
-            <div className="contacts-Box2">
-              <p className="contacts-Text3">Message:</p>
-              <p className="contacts-Text4">*</p>
-            </div>
-            <div className="contacts-Box4">
-              <textarea
-                id="text"
-                name="text"
-                value={text}
-                onChange={handleChange}
-                className="contacts-textarea"
-              />
-            </div>
-          </div>
-
-          <div className="contacts-Button-Div">
-            <button
-              className="contacts-Button1"
-              type="button"
-              onClick={showModal}
-            >
-              <p className="contacts-Button-Text">Reset</p>
-            </button>
-            <button className="contacts-Button2" onClick={openMessage}>
-              <p className="contacts-Button-Text">Send</p>
-            </button>
+          <div className="contacts-Right">
+            <img className="contacts-Image" src={Image1} alt="" />
           </div>
         </div>
-        <Modal
-          title="Confirm to Reset !"
-          open={isModalOpen}
-          onOk={handleOk}
-          onCancel={handleCancel}
-        ></Modal>
-
-        <div className="contacts-Right">
-          <img className="contacts-Image" src={Image1} alt="" />
-        </div>
+        
+        <Footer />
       </div>
     </div>
   );
