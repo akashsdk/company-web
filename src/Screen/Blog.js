38ff21cb4291ebcfd964020f0ad47e790ed1441b
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-14 21:55:52 +0600

    o

--- src/Screen/Blog.js
+++ src/Screen/Blog.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import './Blog.css';
 import Footer from '../Components/Footer';
 
 export default function Blog() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const hasBoxBeenShown = localStorage.getItem("hasBoxBeenShown");
+
+    if (!hasBoxBeenShown) {
+      setIsVisible(true);
+    }
+  }, []);
+
+  const handleClose = () => {
+    setIsVisible(false);
+    localStorage.setItem("hasBoxBeenShown", "true");
+  };
   return (
     <div className="AppBody" >
       <div className="Blog-Body">
         <p>Blog</p>
+        {isVisible && (
+          <div>
+            <p>This is the box content</p>
+            <button onClick={handleClose}>Close</button>
+          </div>
+        )}
         <Footer />
       </div>
       </div>
--- src/Screen/Career.js
+++ src/Screen/Career.js
@@ -3,30 +3,29 @@ import "./Career.css";
 import Footer from "../Components/Footer";
 
 export default function Career() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const hasBoxBeenShown = localStorage.getItem("hasBoxBeenShown");
-
-    if (!hasBoxBeenShown) {
-      setIsVisible(true);
-    }
-  }, []);
-
-  const handleClose = () => {
-    setIsVisible(false);
-    localStorage.setItem("hasBoxBeenShown", "true");
-  };
   return (
     <div className="AppBody">
       <div className="Career-Body">
-        <p>Career</p>
-        {isVisible && (
-          <div>
-            <p>This is the box content</p>
-            <button onClick={handleClose}>Close</button>
+        <div className="Career-Box1">
+          <div className="Career-Box1-Left">
+            <p className="Career-Box1Left-text1">Join with us</p>
+            <p className="Career-Box1Left-text2">
+              {" "}
+              Be a part of super talented team in the fastest growing software
+              development company!
+            </p>
+            <p className="Career-Box1Left-text3">
+              Join our team as a software engineer and experience a dynamic work
+              environment with opportunities for growth. Be part of a passionate
+              and cohesive team delivering innovative software solutions.
+            </p>
+            <button className="Career-Box1Left-Button"> 
+              <p className="Career-Box1Left-text4">Apply Now</p>
+            </button>
           </div>
-        )}
+          <div className="Career-Box1-Right">Right</div>
+        </div>
+
         <Footer />
       </div>
     </div>
