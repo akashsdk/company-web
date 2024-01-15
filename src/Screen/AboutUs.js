36ff7ad400abd4c95462a28a9426bb18786ea4ce
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-16 01:03:55 +0600

    ok

--- src/Screen/AboutUs.js
+++ src/Screen/AboutUs.js
@@ -1,13 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AboutUs.css";
-import Footer from '../Components/Footer';
+import Footer from "../Components/Footer";
 
 export default function AboutUs() {
+  const [page, setPage] = useState(1);
   return (
     <div className="AppBody">
       <div className="About-Body">
-      <h1>AboutUs</h1>
-      <Footer />
+        <h1>AboutUs</h1>
+
+        {page === 1 ? (
+          <div
+            style={{
+              height: "100px",
+              width: "300px",
+              border: "1px solid black",
+            }}
+          >
+            box1
+          </div>
+        ) : page === 2 ? (
+          <div
+            style={{
+              height: "100px",
+              width: "300px",
+              border: "1px solid black",
+            }}
+          >
+            box2
+          </div>
+        ) : page === 3 ? (
+          <div
+            style={{
+              height: "100px",
+              width: "300px",
+              border: "1px solid black",
+            }}
+          >
+            box3
+          </div>
+        ) : page === 4 ? (
+          <div
+            style={{
+              height: "100px",
+              width: "300px",
+              border: "1px solid black",
+            }}
+          >
+            box4
+          </div>
+        ) : (
+          <p>hgygy</p>
+        )}
+
+        <button
+          style={{
+            borderColor: "red",
+            backgroundColor: page === 1 ? "red" : "",
+          }}
+          onClick={() => {
+            setPage(1);
+          }}
+        >
+          Box1
+        </button>
+        <button
+          style={{
+            borderColor: "red",
+            backgroundColor: page === 2 ? "red" : "",
+          }}
+          onClick={() => {
+            setPage(2);
+          }}
+        >
+          Box2
+        </button>
+        <button
+          style={{
+            borderColor: "red",
+            backgroundColor: page === 3 ? "red" : "",
+          }}
+          onClick={() => {
+            setPage(3);
+          }}
+        >
+          Box3
+        </button>
+        <button
+          style={{
+            borderColor: "red",
+            backgroundColor: page === 4 ? "red" : "",
+          }}
+          onClick={() => {
+            setPage(4);
+          }}
+        >
+          Box4
+        </button>
+
+        <Footer />
       </div>
     </div>
   );
