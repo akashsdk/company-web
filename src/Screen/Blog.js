e83bfbd963ce3b073d74b75276cf619580c3b939
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-18 20:11:32 +0600

    ok

--- src/Screen/Blog.js
+++ src/Screen/Blog.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./Blog.css";
 import Footer from "../Components/Footer";
 
+import { Image } from "antd";
+
+import photo1 from '../Image/Profile.jpg';
+
 export default function Blog() {
   return (
     <div className="AppBody">
       <div className="Blog-Body">
         {/* Part-1 */}
         <div className="blogPart1-Body">
-          <div className="blogPart1-left">left</div>
+          <div className="blogPart1-left">
+            <p className="blogPart1-text1">MESSAGE FROM THE CHAIRMAN</p>
+            <div className="blogPart1-div">
+              <Image className="blogPart1-img" src={photo1} />
+            </div>
+          </div>
           <div className="blogPart1-right">right</div>
         </div>
         <Footer />
