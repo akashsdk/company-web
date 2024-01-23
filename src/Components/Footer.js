d5dee343500e027e6bee190472d8126a854aa742
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-23 19:20:21 +0600

    ok

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,75 @@
-import React from 'react'
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import { Button, Dropdown } from "antd";
+import { CaretUpOutlined } from "@ant-design/icons";
+
+const items = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        3rd menu item
+      </a>
+    ),
+  },
+];
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="footer-Body">
+      <div className="footer-Top">
+        <h1>Icon and Name</h1>
+      </div>
+      <div className="footer-Mdi">Mid</div>
+      <div className="footer-Down-Line" />
+      <div className="footer-Down">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} name | All rights
+          reserved.
+        </p>
+        <div>
+          <Dropdown
+            menu={{
+              items,
+            }}
+            placement="topRight"
+            arrow
+          >
+            <button>
+              <p>topRight</p>
+              <CaretUpOutlined />
+            </button>
+          </Dropdown>
+        </div>
+      </div>
+    </div>
+  );
 }
