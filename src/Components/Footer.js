cee81f787bd00ef8ff397ff9b82865b183bd4e84
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-23 23:33:14 +0600

    ok

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Footer.css";
 import { Button, Dropdown } from "antd";
-import { CaretUpOutlined, CaretDownOutlined } from "@ant-design/icons";
+import {
+  CaretUpOutlined,
+  CaretDownOutlined,
+  HomeOutlined,
+  AppstoreOutlined,
+  ContactsOutlined,
+  MacCommandOutlined,
+  DesktopOutlined,
+  CustomerServiceOutlined,
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 const items = [
   {
@@ -50,27 +60,75 @@ export default function Footer() {
       </div>
       <div className="footer-Mdi">
         <div>side</div>
-        <div>Right</div>
+        <div className="footer-Mdi-Box">
+          <div className="footer-Mdi-box1">Box1</div>
+          <div className="footer-Mdi-box2">
+            <div className="footer-MdiBox2-Div">
+              <p className="footer-MdiBox2-Text1">HELP</p>
+              <Link className="footer-MdiBox2-Link">
+                <ContactsOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Authors</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <HomeOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">FAQs</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <AppstoreOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Follow</p>
+              </Link>
+            </div>
+
+            <div className="footer-MdiBox2-Div">
+              <p className="footer-MdiBox2-Text1">COMPANY</p>
+              <Link className="footer-MdiBox2-Link">
+                <HomeOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Home</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <AppstoreOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Services</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <ContactsOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Career</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <MacCommandOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">Blog</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <DesktopOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2"> Contact</p>
+              </Link>
+              <Link className="footer-MdiBox2-Link">
+                <CustomerServiceOutlined className="footer-MdiBox2-Icon" />
+                <p className="footer-MdiBox2-Text2">About Us</p>
+              </Link>
+            </div>
+          </div>
+          <div className="footer-Mdi-box3">box3</div>
         </div>
+      </div>
       <div className="footer-Down-Line" />
       <div className="footer-Down">
-        <p style={{marginLeft:'10px'}}>
+        <p style={{ marginLeft: "10px" }}>
           Copyright &copy; {new Date().getFullYear()} name | All rights
           reserved.
         </p>
-          <Dropdown
-            menu={{
-              items,
-            }}
-            placement="topRight"
-            arrow
-          >
-            <button className="footer-Down-Button">
-              <p className="footer-Down-Text2">English</p>
-              <CaretUpOutlined className="footer-Down-Icon"/>
-              <CaretDownOutlined className="footer-Down-Icon2"/>
-            </button>
-          </Dropdown>
+        <Dropdown
+          menu={{
+            items,
+          }}
+          placement="topRight"
+          arrow
+        >
+          <button className="footer-Down-Button">
+            <p className="footer-Down-Text2">English</p>
+            <CaretUpOutlined className="footer-Down-Icon" />
+            <CaretDownOutlined className="footer-Down-Icon2" />
+          </button>
+        </Dropdown>
       </div>
     </div>
   );
