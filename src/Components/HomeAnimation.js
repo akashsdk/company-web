a0b81bd0a8a4a608c2238ae059bd5a470b37da5a
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-01-27 23:17:54 +0600

    ok

--- src/Components/HomeAnimation.js
+++ src/Components/HomeAnimation.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './HomeAnimation.css';
+import React from "react";
+import "./HomeAnimation.css";
 
 export default function HomeAnimation() {
   return (
-    <div className='h-AnimationBody'>
-        Animation
-        </div>
-  )
+    <div className="h-AnimationBody">
+      <div style={{ height: "20px" }} />
+      <div className="Animation-Div">
+        <p className="Animation-Text1">
+          Our Custom Software Development Products for Partners
+        </p>
+        <p className="Animation-Text2">
+          Our company specializes in software outsourcing and provides robust,
+          scalable, and efficient solutions to clients around the world. We are
+          committed to providing quality services at competitive prices. Our
+          goal is to help our clients achieve their business objectives by
+          providing them with the best possible software solutions.
+        </p>
+      </div>
+    </div>
+  );
 }
