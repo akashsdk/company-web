b313aa6c0f295232191f2d69f284945a8c9f61df
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-02-03 18:53:46 +0600

    ok

--- src/Components/HomeAnimation.js
+++ src/Components/HomeAnimation.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./HomeAnimation.css";
 
+import Img1 from "../Image/web Pic 2.png";
+import Img2 from "../Image/web Pic 3 copy.jpg";
+import Img3 from "../Image/web Pic 4.png";
+import Img4 from "../Image/web Pic 5 copy.jpg";
+
+import { Link } from "react-router-dom";
+import { ArrowRightOutlined } from "@ant-design/icons";
+
 export default function HomeAnimation() {
   return (
     <div className="h-AnimationBody">
@@ -16,6 +24,21 @@ export default function HomeAnimation() {
           goal is to help our clients achieve their business objectives by
           providing them with the best possible software solutions.
         </p>
+
+        <div className="Animation-Box">
+          <div className="Animation-Box-Left">
+            <div className="Animation-ImgDiv">
+              <img className="Animation-Img" src={Img4} alt="" />
+            </div>
+            <p className="Animation-Text3">Name</p>
+            <p className="Animation-Text4">Detels</p>
+            <Link className="Animation-Link">
+              <p className="Animation-Text5">Contact Us</p>
+              <ArrowRightOutlined className="Animation-Icon" />
+            </Link>
+          </div>
+          <div className="Animation-Box-Right">Right</div>
+        </div>
       </div>
     </div>
   );
--- src/Screen/CheckOut.js
+++ src/Screen/CheckOut.js
@@ -14,6 +14,7 @@ import {
   FileProtectOutlined,
   DollarOutlined,
 } from "@ant-design/icons";
+
 import {
   Input,
   Button,
@@ -22,7 +23,6 @@ import {
   Modal,
   Result,
   notification,
-  message,
 } from "antd";
 
 const { TextArea } = Input;
